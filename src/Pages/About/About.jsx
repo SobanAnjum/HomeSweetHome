1a7a32cc7094091f512d1073f6bbb73ee5dbfaca
SobanAnjum <sobananjum0@gmail.com>
2025-02-10 16:24:08 +0500

    created div for icons

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -14,8 +14,6 @@ const About = () => {
                 scrub:1,
                 start:"top 0%",
 
-                markers:true
-
             }
         })
         tl.from('.topHeading',{
@@ -29,11 +27,26 @@ const About = () => {
     <div className='AboutBody'>
         <div className="topHeading">
             <div className="texts">
+                <div className="itemGroup">
+                    
             <h1> <span className='we'>24/7</span> <br /><span className='provide'>SERVICES</span></h1>
+                </div>
+                <div className="itemGroup">
             <h1> <span className='we'>Verified </span> <br /><span className='provide'>Professionals</span></h1>
+
+                </div>
+                <div className="itemGroup">
             <h1> <span className='we'>COMPLETED OVER</span> <br /><span className='provide'>200+ Projects</span></h1>
+
+                </div>
+                <div className="itemGroup">
             <h1> <span className='we'>BEST HANDYMAN</span> <br /><span className='provide'>SERVICES</span></h1>
+
+                </div>
+                <div className="itemGroup">
             <h1> <span className='we'>AT</span> <br /><span className='provide'>HOME SWEET HOME</span> <br />WE HAVE</h1>
+
+                </div>
 
             </div>
         </div>
