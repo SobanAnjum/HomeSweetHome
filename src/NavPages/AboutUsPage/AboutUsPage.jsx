48f28066832ca41bcf984998d32f897df06f3a1f
SobanAnjum <Sobananjum0@gmail.com>
2025-03-13 22:28:20 +0300

    About Page initiation

--- src/NavPages/AboutUsPage/AboutUsPage.jsx
+++ src/NavPages/AboutUsPage/AboutUsPage.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import './AboutUsPage.css'
 import Nav from '../../Components/Nav/Nav'
+import Footer from '../../Components/Footer/Footer'
 
 const AboutUsPage = () => {
 return (
     <div>
         <Nav/>
-        <h1>AboutUsPage</h1>
+        <div className="AboutUsBody">
+            <div className="Head">
+
+            <h1 className='AUHead' >ABOUT OUR ORGANIZATION</h1>
+            <div className='AboutDescription'>
+                <h1>Welcome to wefixuaehome.com</h1>
+                <p>Your trusted partner for all home maintenance and repair needs across the UAE. We are a team of skilled professionals dedicated to delivering high-quality and reliable services, ensuring your home remains in top condition, no matter the issue.
+
+At WeFixUAEHome, we understand the importance of a comfortable and safe living space. That's why we offer a wide range of services designed to address every aspect of home maintenance—from plumbing and electrical repairs to painting, AC servicing, and more. With a customer-first approach, we are committed to providing affordable solutions, fast response times, and expert craftsmanship.</p>
+            </div>
+            </div>
+        </div>
+        <Footer/>
+
     
     </div>
 )
